144226ffeec72167d64a4ddc17770c5291196a55
Erfan Ajorlu <erfanajoglu098@gmail.com>
2024-01-04 21:19:37 +0330

    get users number with write query

--- index.js
+++ index.js
@@ -6,29 +6,39 @@ mongoose
     console.log("connect to mongo...");
   })
   .catch((err) => {
-    console.log('can not connected....')
+    console.log("can not connected....");
   });
 
 const userSchema = mongoose.Schema({
-    first_name : String,
-    last_name : {type : String, required : true},
-    favorites : [String],
-    data : {type : Date , default: Date.now()},
-    admin : Boolean
+  first_name: String,
+  last_name: { type: String, required: true },
+  favorites: [String],
+  data: { type: Date, default: Date.now() },
+  admin: Boolean,
 });
 
-const User = mongoose.model('User' , userSchema);
+const User = mongoose.model("User", userSchema);
 
-async function createUser(){
-    const user = new User({
-        first_name : "Erfan",
-        last_name : "Ajorlu" ,
-        favorites : [ 'computer science', 'soccer' , 'game' , 'music' , 'movie'],
-        admin:true
-    });
+async function createUser() {
+  const user = new User({
+    first_name: "Erfan",
+    last_name: "Ajorlu",
+    favorites: ["computer science", "soccer", "game", "music", "movie"],
+    admin: true,
+  });
 
-    const result = await user.save();
-    console.log(result);
+  const result = await user.save();
+  console.log(result);
 }
 
-createUser();
+async function getUsers() {
+  const users = await User.find({ first_name: "Erfan" })
+    .limit(5)
+    .sort({ first_name: -1 })
+    .select({first_name : 1 , last_name : 1})
+    .count();
+  console.log(users);
+}
+
+getUsers();
+// createUser();
