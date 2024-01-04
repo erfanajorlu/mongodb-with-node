51f53a9a978c9673de1f90cf8bd71b42473f62fd
Erfan Ajorlu <erfanajoglu098@gmail.com>
2024-01-04 21:15:02 +0330

    send data to mongodb

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ mongoose
   .catch((err) => {
     console.log('can not connected....')
   });
+
+const userSchema = mongoose.Schema({
+    first_name : String,
+    last_name : {type : String, required : true},
+    favorites : [String],
+    data : {type : Date , default: Date.now()},
+    admin : Boolean
+});
+
+const User = mongoose.model('User' , userSchema);
+
+async function createUser(){
+    const user = new User({
+        first_name : "Erfan",
+        last_name : "Ajorlu" ,
+        favorites : [ 'computer science', 'soccer' , 'game' , 'music' , 'movie'],
+        admin:true
+    });
+
+    const result = await user.save();
+    console.log(result);
+}
+
+createUser();
